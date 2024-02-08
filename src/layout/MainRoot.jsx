6b11958c1abff92adb69008a5843aaf3f969d9d3
mizan-chowdhury <mizan1034@gmail.com>
2024-02-08 23:09:52 +0600

    completed footer section

--- src/layout/MainRoot.jsx
+++ src/layout/MainRoot.jsx
@@ -1,14 +1,15 @@
 import { Outlet } from "react-router-dom";
 import Navbar from "../pages/components/Navbar";
+import Footer from "../pages/components/Footer";
 
 const MainRoot = () => {
     return (
         <div>
             <Navbar></Navbar>
-            <div className="max-w-screen-2xl mx-auto">
+            <div className="max-w-screen-2xl mx-auto py-10">
                 <Outlet></Outlet>
             </div>
-
+        <Footer></Footer>
         </div>
     );
 };
